b8016b1d0103dbc57715934587902cd7bd3e0b1a
Ivan Jorge <ivan.jorge001@zoho.com>
2024-01-26 23:14:41 -0500

    complete

--- test/getUserInfo.ts
+++ test/getUserInfo.ts
@@ -8,16 +8,3 @@ export interface UserInfo {
 export function GetUserInfo(params: UserInfo) {
   
 }
-
-// { 
-//   description: 'this gets user informations',
-//   name: 'get_user_info',
-//   parameters: 'JSONSchema'
-// }
-
-// 1. Pick the function name 
-// 2. Description 
-// 3. Param Type 
-// 4. Return Type
-// 5. Convert Param and Return Type to JSON Schema
-// 6. out put a file with this data 
